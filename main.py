d601fdc942506ed2c0ac7a98668b630cd506060e
Synapt1x <chriscadonic@gmail.com>
2015-11-02 14:03:25 -0600

    Add work notes from November 2

--- main.py
+++ main.py
@@ -15,7 +15,21 @@ OUT:
 
 '''
 # Import relevant packages
+import sys
+import numpy as np # required for pandas
+import pandas as pd # import pandas data structures (dataFrame) and read_excel
+
+# Initialize variables
+xCoords = {}
+yCoords = {}
+peckNum = {}
+trial = {}
 
 # First read-in the data
+file = open('data/test.xls')
 
-# 
+# now read excel file data
+pigeonData = pd.read_excel(file)
+
+# parse the data and separate columns into series'
+
