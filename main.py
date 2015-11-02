2c686168b7fc46fb0a9d20d09feae97388b36277
Synapt1x <chriscadonic@gmail.com>
2015-11-02 13:21:16 -0600

    Add worklog folder and first elements of main

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+Data Processor
+============================
+Created by: Chris Cadonic
+For: Dr. Debbie Kelly lab
+----------------------------
+This program was developed to automatically
+format input excel data for statistical
+analysis in the statistical analysis
+software.
+
+IN:
+
+OUT:
+
+'''
+# Import relevant packages
+
+# First read-in the data
+
+# 
