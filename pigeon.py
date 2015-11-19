016e72b1d96edd7dd822dff4ef7a02f76c0562a9
Synapt1x <chriscadonic@gmail.com>
2015-11-19 15:46:26 -0600

    Add work on calcDist and worklog
    Added work on calcDist, which now successfully calculates the
    distance between every peck and its respective goal. The series
    containing this information was successfully added to the main
    data frame but I noticed there are errors in adding Series to the
    data frame since there are now "Unnamed" columns.

--- pigeon.py
+++ pigeon.py
@@ -16,6 +16,7 @@ OUT:
 
 import numpy as np
 import pandas as pd
+from math import sqrt
 
 # define the class for pigeon objects
 class Pigeon:
@@ -28,7 +29,7 @@ class Pigeon:
 
         # separate Trial Information into separate columns
         colnames = ['Pigeon Name', 'Experiment Phase', 'Session', 'Trial', 'Trial Type']  #name the columns
-        tempDf=pd.DataFrame(dataframe['Trial Information'].str.split('_').tolist(),columns=colnames)
+        tempDf=pd.DataFrame(dataframe['Trial Information'].str.split('_').tolist(),columns=colnames) # make a table with these columns to be added to self.dataframe
 
         # remove calibration coeffient and trial information columns
         self.dataframe = self.dataframe.drop(['Calibration Coefficient', 'Trial Information'], axis=1)
@@ -38,31 +39,78 @@ class Pigeon:
 
         return self.dataframe
 
-    def __init__(self, data):  # define the constructor for when pigeons are
-        # made
-        self.dataframe = data
-
-        self.dataframe = self.processFrame(self.dataframe)
-
-        print self.dataframe
-
     def findGoals(self, goalType):  # method for finding the indices
         # corresponding to goals
         # find indices where allPecks has the word goal
-        self.indices = np.where(self.dataframe['Peck'] == goalType)[0]
+        goalIndices = np.where(self.dataframe['Peck'] == goalType)[0]
 
         # extract the x and y co-ordinates of each goal
-        xGoals = self.dataframe['X'][self.indices]
-        yGoals = self.dataframe['Y'][self.indices]
+        xGoals = self.dataframe['X'][goalIndices]
+        yGoals = self.dataframe['Y'][goalIndices]
 
-        return (xGoals, yGoals)
+        return (xGoals, yGoals, goalIndices)
 
     def parseTrialInfo(self):  # method for parsing trial information series
         # find each trial based on when a new goal is defined
         return 'parse'
 
     def calcDist(self):  # method for calculating euclidean distance from goals
-        return 'calc'
+        # call findGoals to determine the (x,y,indices) of the goals
+        (self.goalsX, self.goalsY, self.goalIndices) = self.findGoals('goal')
+        (self.OppsX, self.OppsY, self.OppsIndices) = self.findGoals('Opp goal')
+        (self.AFsX, self.AFsY, self.AFIndices) = self.findGoals('AF goal')
+
+        # create enumerator object to go over all goals and index them
+        peckIterator = enumerate(self.goalIndices)
+
+        # initialize placeholder series to dynamically append calc for each goal
+        allDists = pd.Series([])
+
+        # iterate over all goals with an index for loop number (i.e. trial num)
+        for goalNum, peckIndex in peckIterator:
+            # define first peck index for this particular goal
+            firstPeck = peckIndex + 1
+            # use try-except to define last index for goal, except catches the end of the series and sets the final lastPeck to the last poss. ind
+            try:
+                lastPeck = self.goalIndices[goalNum+1]
+            except IndexError:
+                lastPeck = len(self.dataframe)
+
+            # Create range for the indices of pecks for this goal
+            peckRange = range(firstPeck, lastPeck)
+
+            # Check if Opp goal and/or AF goal are defined, if so then remove them from peckRange as they are currently included at the start
+            if (peckIndex+1 in self.OppsIndices):
+                del peckRange[0]
+            if (peckIndex+2 in self.AFIndices):
+                del peckRange[0]
+
+            # Acquire all the peck locations for this trial
+            xPecks = self.dataframe['X'][peckRange]
+            yPecks = self.dataframe['Y'][peckRange]
+
+            # Find each direct x and y distance from goal
+            xDists = xPecks.apply(lambda x: x - self.goalsX[peckIndex])
+            yDists = yPecks.apply(lambda y: y - self.goalsY[peckIndex])
+
+            # Calculate the Euclidean distance for each peck from goal
+            totalDist = xDists*xDists + yDists*yDists
+            finalDists = totalDist.apply(sqrt)
+
+            # store all distances in Series to be added to data frame
+            allDists = pd.concat([allDists,finalDists], axis=0)
+
+        # Add all peck distances to main data frame
+        self.dataframe['Distance To Main Goal'] = allDists
+
+        # Set all NaN's to 'goal'
+        self.dataframe = self.dataframe.fillna('goal')
 
     def formatOuput(self):  # method for summarizing and formatting output data
         return 'format'
+
+    def __init__(self, data):  # define the constructor for when pigeons are
+        # made
+        self.dataframe = data
+
+        self.dataframe = self.processFrame(self.dataframe)
