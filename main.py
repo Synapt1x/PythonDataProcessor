07deb3129b1e76cf72eb52544eaa5aee0e79394e
Synapt1x <chriscadonic@gmail.com>
2016-06-01 14:36:04 -0500

    Fix numpy pandas dependencies

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ dirname, mainFile = path.split(path.abspath('__file__'))
 outputFilename = path.join(dirname,'output.xls')
 
 # Ask user to identify the data directory
-while True:
+'''while True:
     try:
         dataDirname = tkFileDialog.askdirectory(parent=root,
             initialdir="/",title='Please select the data directory.')
@@ -59,8 +59,9 @@ while True:
         break
     except ValueError:
         print "Please select a valid directory..."
+'''
 
-#dataDirname = '/home/synapt1x/Projects/DrKellyProject/data'
+dataDirname = '/home/synapt1x/Projects/DrKellyProject/data'
 
 # cd to data directory
 chdir(dataDirname)
